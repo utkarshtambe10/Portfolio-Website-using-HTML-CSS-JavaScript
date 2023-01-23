d60fb73465f3cc7baada925a38e9622471ca12c1
Utkarsh Tambe <utkarsh.tambe33@gmail.com>
2023-01-23 22:16:14 +0530

    building tips page

--- assets/js/main.js
+++ assets/js/main.js
@@ -107,3 +107,72 @@ app.about = {
         }, 1000);
     },
 };
+
+app.tips = {
+    init: function () {
+        $(".tips h1").blast({
+            delimiter: "character",
+            tag: "span",
+        });
+        a = 0;
+        $(".tips h1 .blast").each(function () {
+            var el = $(this);
+            setTimeout(function () {
+                el.addClass("animated bounceIn");
+            }, a);
+            a = a + 100;
+        });
+        setTimeout(function () {
+            $(".tips .blast").removeClass("animated bounceIn");
+            $(".tips .blast").css("opacity", 1);
+            $(".tips .blast").mouseenter(function () {
+                var el = $(this);
+                $(this).addClass("animated rubberBand");
+                $(this).one(
+                    "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+                    function () {
+                        el.removeClass("animated rubberBand");
+                    }
+                );
+            });
+        }, 1000);
+    },
+};
+
+app.tipsLogo = {
+    init: function () {
+        (function () {
+            $.ajax({
+                'async': false,
+                'global': false,
+                'url': './data.json',
+                'dataType': "json",
+                'success': function (data) {
+                    data.posts.forEach(function (post) {
+                        var container = document.getElementById('tips-post-container');
+                        var innerDiv = document.createElement('div');
+                        innerDiv.setAttribute('class', 'inner-div-container');
+                        var par = document.createElement('p');
+                        par.setAttribute('class', 'tips-text');
+                        par.textContent = post.description;
+
+                        innerDiv.appendChild(par);
+
+                        var link = document.createElement('a');
+                        link.setAttribute('target', '_blank');
+                        link.setAttribute('href', post.url);
+
+                        var img = document.createElement('img');
+                        img.setAttribute('class', 'tips-image');
+                        img.setAttribute('src', post.src);
+                        link.appendChild(img);
+                        innerDiv.appendChild(link);
+                        container.appendChild(innerDiv);
+                    });
+                }
+            });
+        })();
+        new SimpleBar(document.getElementById('tips-post-container'));
+    },
+};
+
