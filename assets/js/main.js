e63469b84d68f28a296deb05667aee3fd374cdd2
Utkarsh Tambe <utkarsh.tambe33@gmail.com>
2023-01-23 22:29:03 +0530

    building skills page

--- assets/js/main.js
+++ assets/js/main.js
@@ -339,3 +339,63 @@ app.gallery = {
         }, 2000);
     },
 };
+
+app.skills = {
+    init: function () {
+        $(".skills h1").blast({
+            delimiter: "character",
+            tag: "span",
+        });
+        a = 0;
+        $(".skills h1 .blast").each(function () {
+            var el = $(this);
+            setTimeout(function () {
+                el.addClass("animated bounceIn");
+            }, a);
+            a = a + 100;
+        });
+        setTimeout(function () {
+            $(".skills .blast").removeClass("animated bounceIn");
+            $(".skills .blast").css("opacity", 1);
+            $(".skills .blast").mouseenter(function () {
+                var el = $(this);
+                $(this).addClass("animated rubberBand");
+                $(this).one(
+                    "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+                    function () {
+                        el.removeClass("animated rubberBand");
+                    }
+                );
+            });
+        }, 2000);
+        var textCol = "#ffffff";
+        if ($("body").hasClass("white")) {
+            textCol = "#FE0853";
+        }
+        if (
+            !$("#myCanvas").tagcanvas({
+                textColour: textCol,
+                outlineThickness: 0.5,
+                outlineColour: "#FE0853",
+                maxSpeed: 0.06,
+                freezeActive: true,
+                shuffleTags: true,
+                shape: "sphere",
+                zoom: 0.9,
+                noSelect: true,
+                textFont: null,
+                pinchZoom: true,
+                freezeDecel: true,
+                fadeIn: 3000,
+                initial: [0.3, -0.1],
+                depth: 1.4,
+            })
+        ) {
+            $("#myCanvasContainer").hide();
+        }
+        setTimeout(function () {
+            alertify.log(msg8);
+        }, 2000);
+    },
+};
+
